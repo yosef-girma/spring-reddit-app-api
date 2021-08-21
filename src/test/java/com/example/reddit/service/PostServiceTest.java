aea8d022132444bc0954c5a772e7dbc040c16907
Yosef Girma <zanonymouscity@gmail.com>
2021-08-21 21:12:26 +0300

    add save post test

--- src/test/java/com/example/reddit/service/PostServiceTest.java
+++ src/test/java/com/example/reddit/service/PostServiceTest.java
@@ -1,33 +1,47 @@
 package com.example.reddit.service;
 
+import com.example.reddit.dto.PostRequest;
 import com.example.reddit.dto.PostResponse;
 import com.example.reddit.mapper.PostMapper;
 import com.example.reddit.models.Post;
+import com.example.reddit.models.Subreddit;
+import com.example.reddit.models.User;
 import com.example.reddit.repository.PostRepository;
 import com.example.reddit.repository.SubredditRepository;
 import com.example.reddit.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.Mapper;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
-    private final PostRepository postRepository = Mockito.mock(PostRepository.class);
-    private final SubredditRepository subredditRepository = Mockito.mock(SubredditRepository.class);
-    private final UserRepository userRepository = Mockito.mock(UserRepository.class);
-    private final AuthService authService = Mockito.mock(AuthService.class);
-    private final PostMapper postMapper = Mockito.mock(PostMapper.class);
+    @Mock
+    private  PostRepository postRepository;
+    @Mock
+    private  SubredditRepository subredditRepository;
+    @Mock
+    private  UserRepository userRepository;
+    @Mock
+    private AuthService authService ;
+    @Mock
+    private PostMapper postMapper;
+    @Captor
+    private  ArgumentCaptor<Post>  postArgumentCaptor;
 
     @Test
     @DisplayName("Should find post by id")
-    void shouldFindPostById() {
+    void findPostById() {
         PostService postService = new PostService(postRepository,subredditRepository,userRepository,authService,postMapper);
 
         Post post = new Post(123L, "First Post", "http://url.site", "Test",
@@ -41,6 +55,36 @@ class PostServiceTest {
         PostResponse actualPostResponse = postService.getPostById(123L);
         assertThat(actualPostResponse.getId()).isEqualTo(expectedPostResponse.getId());
         assertThat(actualPostResponse.getPostName()).isEqualTo(expectedPostResponse.getPostName());
+
+    }
+
+    @Test
+    @DisplayName("Should save post ")
+    void shouldSavePost(){
+        PostService postService = new PostService(postRepository, subredditRepository, userRepository, authService, postMapper);
+
+        User user = new User(12L,"hello","pass","yoef.gkel@gmail.com",Instant.now(),false);
+        PostRequest postRequest = new PostRequest(1L,"test1","post name","http://hell.com","description");
+        Subreddit subreddit = new Subreddit(123L, "First Subreddit", "Subreddit Description", emptyList(), Instant.now(), user);
+
+        Post post = new Post(123L, "First Post", "http://url.site", "Test",
+                0, null, Instant.now(), null);
+
+
+        Mockito.when(subredditRepository.findByName(postRequest.getSubredditName())).thenReturn(Optional.of(subreddit));
+
+        Mockito.when(postMapper.map(postRequest,subreddit,user)).thenReturn(post);
+        Mockito.when(authService.getCurrentUser()).thenReturn(user);
+        postService.savePost(postRequest);
+
+        Mockito.verify(postRepository,Mockito.times(1))
+                .save(postArgumentCaptor.capture());
+
+        postArgumentCaptor.getValue();
+        assertThat(post.getPostId()).isEqualTo(123L);
+        assertThat(post.getPostName()).isEqualTo("First Post");
+
+
 
     }
 }
